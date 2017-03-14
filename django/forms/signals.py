4c4b5b3b56276a1d9d1ce7da82988a6af83c4b60
David Seddon <david.seddon@growthstreet.co.uk>
2017-03-14 17:30:40 +0000

    Removed cleaned_data from post_clean signal

--- django/forms/signals.py
+++ django/forms/signals.py
@@ -3,6 +3,6 @@ from django.dispatch import Signal
 
 post_init = Signal(providing_args=["form"])
 
-post_clean = Signal(providing_args=["form", "cleaned_data"])
+post_clean = Signal(providing_args=["form"])
 
 post_save = Signal(providing_args=["form"])
